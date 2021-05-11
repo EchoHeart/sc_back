a9ce3f880b7df306f100a68082e5c888e8414eb3
胡万龙 <18146856052@163.com>
2021-05-11 12:14:43 +0800

    部署操作，你们了解下

--- src/main/java/com/example/sc_back/ScBackApplication.java
+++ src/main/java/com/example/sc_back/ScBackApplication.java
@@ -3,6 +3,11 @@ package com.example.sc_back;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 
 @MapperScan("com.example.sc_back.dao")
 @SpringBootApplication
@@ -11,4 +16,5 @@ public class ScBackApplication {
     public static void main(String[] args) {
         SpringApplication.run(ScBackApplication.class, args);
     }
+
 }
