c923dcd9c0d6bfef1a4dd6440286dcb8de0cb7d3
胡万龙 <18146856052@163.com>
2021-05-22 16:57:28 +0800

    使用服务器数据库+调整webConfig多余引用

--- src/main/java/com/example/sc_back/util/WebConfig.java
+++ src/main/java/com/example/sc_back/util/WebConfig.java
@@ -1,17 +1,8 @@
 package com.example.sc_back.util;
 
-import org.springframework.boot.web.server.ConfigurableWebServerFactory;
-import org.springframework.boot.web.server.ErrorPage;
-import org.springframework.boot.web.server.WebServerFactoryCustomizer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 //全局配置类--配置跨域请求 前后端耦合后就不用跨域，酌情注释
 @Configuration
